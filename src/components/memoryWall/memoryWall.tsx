0de9347d1ebed36757c1fef7534be6ec0af79ad9
Spencer Paulmark <fdoou1@gmail.com>
2019-03-23 23:07:14 -0400

    Finish Memory wall demo

--- src/components/memoryWall/memoryWall.tsx
+++ src/components/memoryWall/memoryWall.tsx
@@ -5,9 +5,23 @@ export interface IMemoryWallProps {
   readonly houseguests: Houseguest[];
 }
 
+function componentToHex(c: any) {
+  var hex = Math.round(c).toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r: any, g: any, b: any) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 export function MemoryWall(props: IMemoryWallProps): JSX.Element {
   return <div className="box memory-wall">{getPlayers(props)}</div>;
 }
+
+const maxPopularity = { r: 137, g: 252, b: 137 };
+const minPopularity = { r: 252, g: 137, b: 137 };
+
+// TODO: make portrait its own functional component
 
 function getPlayers(props: IMemoryWallProps): any {
   if (!props.houseguests || props.houseguests.length === 0) {
@@ -24,8 +38,21 @@ function getPlayers(props: IMemoryWallProps): any {
     const imageClass =
       houseguest.isEvicted && evictedImageURL === "BW" ? "grayscale" : "";
 
+    const percent = (houseguest.popularity + 1) / 2;
+    const backgroundColor = houseguest.isEvicted
+      ? undefined
+      : rgbToHex(
+          maxPopularity.r + percent * (minPopularity.r - maxPopularity.r),
+          maxPopularity.g + percent * (minPopularity.g - maxPopularity.g),
+          maxPopularity.b + percent * (minPopularity.b - maxPopularity.b)
+        );
+
     rows.push(
       <div
+        key={houseguest.profileData.name}
+        style={{
+          backgroundColor
+        }}
         className={`memory-wall-portrait ${
           houseguest.isEvicted ? "evicted" : ""
         }`}
@@ -37,7 +64,6 @@ function getPlayers(props: IMemoryWallProps): any {
         />
         <br />
         {houseguest.profileData.name}
-        {/* TODO: Color the name based on popularity. */}
       </div>
     );
   });
--- src/model/houseguest.tsx
+++ src/model/houseguest.tsx
@@ -4,10 +4,13 @@ export class Houseguest {
   readonly profileData: PlayerProfile;
   public isEvicted: boolean = false;
   public isJury: boolean = false;
+  // Popularity ranges from -1 to 1 TODO: fixed at zero
+  public popularity: number = Math.random() * 2 - 1;
   // relationships?
   // descision making functions...
 
   constructor(init: PlayerProfile) {
     this.profileData = init;
+    this.isEvicted = Math.random() > 0.4;
   }
 }
