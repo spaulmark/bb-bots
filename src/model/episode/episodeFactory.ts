b4db97b52b887979fb74f8df7b1b49d100d79d14
Spencer Paulmark <fdoou1@gmail.com>
2019-04-03 22:17:14 -0400

    outline next episode

--- src/model/episode/episodeFactory.ts
+++ src/model/episode/episodeFactory.ts
@@ -1,10 +1,29 @@
 import { GameState } from "../gameState";
 import { hashcode } from "../../utils/hashcode";
+import { Episode } from "./episodeLibrary";
 var MersenneTwister = require("mersenne-twister");
 export class EpisodeFactory {
   private rng: any;
   //ie. "Week 1", "Double Eviction"
   readonly title: string;
+
+  public nextEpisode(gameState: GameState): [Episode, GameState] {
+    throw new Error("todo");
+
+    // run it through the "status/memory wall" page
+
+    // run the gamestate through the HoH Competition function
+
+    // then through the nomination ceremony function
+
+    // then to the veto competition
+
+    // and then the veto ceremony
+
+    // and then the live eviction.
+
+    // return [null, null];
+  }
 
   public constructor(init: GameState, title: string) {
     // generate seed
--- src/model/episode/episodeFragment.tsx
+++ src/model/episode/episodeFragment.tsx
@@ -1,5 +1,8 @@
+import { GameState } from "../gameState";
+
 export class EpisodeFragment {
   readonly title: string = "";
+  readonly gameState!: GameState; // TODO: for generating graphs on the side.
   readonly content: JSX.Element = <div />;
   public constructor(init: EpisodeFragment) {
     Object.assign(this, init);
