05d61e6c86667b5ba45b5a6e783865d90b54f95d
Spencer Paulmark <fdoou1@gmail.com>
2019-04-06 12:17:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bb-bots/precache-manifest.0c1d7d132cc518e24a155cc057fa9dd7.js"
+  "/bb-bots/precache-manifest.1d41a324caaad20574e12b2bd60495bd.js"
 );
 
 workbox.clientsClaim();
