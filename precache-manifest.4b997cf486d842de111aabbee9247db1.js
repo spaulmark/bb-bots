8043a2776593bce772189d2770b20d246e39c792
Spencer Paulmark <fdoou1@gmail.com>
2019-05-24 22:26:34 -0400

    Updates

---
+++ precache-manifest.4b997cf486d842de111aabbee9247db1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a78736eeef7e5f831be",
+    "url": "/bb-bots/static/js/runtime~main.ee1276bc.js"
+  },
+  {
+    "revision": "35619b6c84256dbc9b2a",
+    "url": "/bb-bots/static/js/main.f579f8d8.chunk.js"
+  },
+  {
+    "revision": "a7734edb3a664e5ebd72",
+    "url": "/bb-bots/static/js/2.40d89cda.chunk.js"
+  },
+  {
+    "revision": "35619b6c84256dbc9b2a",
+    "url": "/bb-bots/static/css/main.0b11e494.chunk.css"
+  },
+  {
+    "revision": "82f8959f91bb73f17696d12b29b286c7",
+    "url": "/bb-bots/index.html"
+  }
+];
