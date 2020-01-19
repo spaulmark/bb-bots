3eb141b2e9b04fe6cf58a0ad1915aa8ce9127b52
spaulmark <fdoou1@gmail.com>
2020-01-18 19:48:54 -0500

    Make the jury vote scene look nice

--- src/components/episode/scenes/juryVoteScene.tsx
+++ src/components/episode/scenes/juryVoteScene.tsx
@@ -1,41 +1,49 @@
 import { GameState, getJurors, nonEvictedHouseguests } from "../../../model";
 import { Scene } from "../scene";
 import { castJuryVote } from "../../../utils/ai/aiApi";
-import { Portrait } from "../../playerPortrait/portraits";
+import { Portraits } from "../../playerPortrait/portraits";
 import React from "react";
+import { ProfileHouseguest } from "../../memoryWall";
+import { CenteredBold } from "../../layout/centered";
+import { DividerBox } from "../../layout/box";
+import { NextEpisodeButton } from "../../nextEpisodeButton/nextEpisodeButton";
 
 export function juryVoteScene(initialGameState: GameState): Scene {
     const jurors = getJurors(initialGameState);
     const finalists = nonEvictedHouseguests(initialGameState);
-    let voteCount = [0, 0];
-    const votes = jurors.map(juror => {
-        const result = castJuryVote(juror, finalists);
-        voteCount[result]++;
-        return result;
+    const votes: Array<ProfileHouseguest[]> = [[], []];
+
+    jurors.forEach(juror => {
+        const decision = castJuryVote(juror, finalists);
+        const result: ProfileHouseguest = { ...juror };
+        votes[decision].push(result);
     });
-    const voteBlocks = [];
+    const votesFor0 = votes[0].length;
+    const votesFor1 = votes[1].length;
+    const isUnanimous = votesFor0 === 0 || votesFor1 === 0;
+    const voteCountText = isUnanimous
+        ? "By a unanimous vote..."
+        : `By a vote of ${votesFor0} to ${votesFor1}...`;
 
-    for (let i = 0; i < votes.length; i++) {
-        voteBlocks.push(
-            <div className="columns" key={`jury-vote${i}`}>
-                <Portrait houseguest={jurors[i]} />
-                <p>
-                    <b>{`${jurors[i].name} has voted for...`}</b>
-                </p>
-                <Portrait houseguest={finalists[votes[i]]} />
-            </div>
-        );
-    }
-    const winner = voteCount[0] > voteCount[1] ? finalists[0] : finalists[1];
+    const winner = votesFor0 > votesFor1 ? finalists[0] : finalists[1];
 
-    const scene: Scene = new Scene({
+    const scene = new Scene({
         title: "Jury Votes",
         gameState: initialGameState,
         content: (
             <div>
-                {voteBlocks}
-                <Portrait houseguest={winner} />
-                {`Congratulations, ${winner.name}, you are the winner of Big Brother!!!`}
+                <CenteredBold>{voteCountText}</CenteredBold>
+                <div className="columns is-centered">
+                    <DividerBox className="column">
+                        <Portraits houseguests={votes[0]} centered={true} />
+                    </DividerBox>
+                    <DividerBox className="column">
+                        <Portraits houseguests={votes[1]} centered={true} />
+                    </DividerBox>
+                </div>
+                <Portraits houseguests={finalists} centered={true} />
+                <CenteredBold>{`Congratulations, ${winner.name}... you are the winner of Big Brother!`}</CenteredBold>
+                <NextEpisodeButton />
             </div>
         )
     });
