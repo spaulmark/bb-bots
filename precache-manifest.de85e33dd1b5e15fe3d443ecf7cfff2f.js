a52f1be0c7f7ad888e022b7231c559ee4c7468ab
spaulmark <fdoou1@gmail.com>
2020-04-15 20:02:29 -0400

    Updates

--- precache-manifest.de85e33dd1b5e15fe3d443ecf7cfff2f.js
+++ precache-manifest.de85e33dd1b5e15fe3d443ecf7cfff2f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bb-bots/static/js/runtime~main.ee1276bc.js"
   },
   {
-    "revision": "968ae21ef7ec2393b052",
-    "url": "/bb-bots/static/js/main.4a3b5cff.chunk.js"
+    "revision": "07bfa9b8bca2e2bfb658",
+    "url": "/bb-bots/static/js/main.b4f8835a.chunk.js"
   },
   {
     "revision": "8f667ee9240cf68f9090",
     "url": "/bb-bots/static/js/2.da7a2fe7.chunk.js"
   },
   {
-    "revision": "968ae21ef7ec2393b052",
+    "revision": "07bfa9b8bca2e2bfb658",
     "url": "/bb-bots/static/css/main.89f690db.chunk.css"
   },
   {
-    "revision": "8ac9e52b2db866ec6a5224c172f75648",
+    "revision": "3aac232ef3a07be8857de125f224239d",
     "url": "/bb-bots/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bb-bots/precache-manifest.7a94e2e98c40981db5bfb0f15152bfa2.js"
+  "/bb-bots/precache-manifest.de85e33dd1b5e15fe3d443ecf7cfff2f.js"
 );
 
 workbox.clientsClaim();
