c1cd030d08fedc23d53d524d65ed2212bae9d790
Spencer <fdoou1@gmail.com>
2022-03-02 21:59:22 -0500

    clean up comments

--- src/utils/generateCliques.ts
+++ src/utils/generateCliques.ts
@@ -9,8 +9,6 @@ export interface Graph {
     neighbors: (v: number) => Set<number>;
 }
 
-// TODO: we want core [] then affiliates [[], []].
-
 export function newCliquesLength(a: Cliques): number {
     return a.core.length + (a.affiliates ? a.affiliates[0].length + a.affiliates[1].length : 0);
 }
@@ -26,17 +24,12 @@ export function generateCliques(gameState: GameState): Cliques[] {
     bronKerbosch(new Set<number>([]), new Set(g.nodes), new Set<number>([]), g);
     cliques.forEach((clique) => clique.map((id) => getById(gameState, id)));
     cliques = cliques.sort((a, b) => b.length - a.length);
-    // merge cliques that are similar *enough*
-    // i think you do this by merging cliques with the most overlap [until when?]
     const result: Cliques[] = [];
-    // clique i, clique j, overlap size
+    // ids of clique i, clique j, & overlap size
     const mergeCandidates: [number, number, Cliques][] = [];
     // generate overlap numbers for each clique (n^2)
-    // if the overlap is significant enough, add it to the merge queue
     cliques.forEach((clq, i) => {
         const cliqueI = new Set(clq);
-        // TODO: an optimization is possible to kill this for loop early.
-        // when the coreSize large enough condition will be impossible.
         for (let j = i + 1; j < cliques.length; j++) {
             const cliqueJ = new Set(cliques[j]);
             const core = intersection(cliqueI, cliqueJ);
@@ -77,91 +70,10 @@ export function generateCliques(gameState: GameState): Cliques[] {
         result.push({ core: clique });
     });
 
-    // TODO: do some insane madman lunatic merging based on duplicate affiliates
-
     // then sort and return result
     result.sort((a: Cliques, b: Cliques) => newCliquesLength(b) - newCliquesLength(a));
 
-    // render with → and ←
-    // ie. [affiliatesL] → [core] ← [affiliatesR]
-
     return result;
-}
-
-// function oldgenerateCliques(gameState: GameState): Cliques[] {
-//     const g = generateGraph(gameState);
-//     cliques = [];
-//     bronKerbosch(new Set<number>([]), new Set(g.nodes), new Set<number>([]), g);
-//     cliques.forEach((clique) => clique.map((id) => getById(gameState, id)));
-//     cliques = cliques.sort((a, b) => b.length - a.length);
-//     // merge cliques that have a difference of only one person
-//     const result: Cliques[] = [];
-//     const blacklist: Set<number> = new Set<number>();
-//     cliques.forEach((clq, i) => {
-//         if (blacklist.has(i)) return;
-//         if (clq.length === 1) {
-//             result.push({ core: clq, affiliates: [] });
-//             return;
-//         }
-//         const cliqueI = new Set(clq);
-//         let cliquePushed: boolean = false;
-//         for (let j = i + 1; j < cliques.length && cliques[j].length === clq.length; j++) {
-//             const cliqueJ = new Set(cliques[j]);
-//             const core: Set<number> = intersection(cliqueI, cliqueJ);
-//             if (core.size === cliqueI.size - 1) {
-//                 const affiliates: number[] = Array.from(difference(cliqueJ, core));
-//                 result.push({ core: clq, affiliates });
-//                 cliquePushed = true;
-//                 blacklist.add(j);
-//             }
-//         }
-//         if (!cliquePushed) {
-//             result.push({ core: clq, affiliates: [] });
-//         }
-//     });
-//     // Merge duplicate alliances together
-//     result.forEach((clq, i) => {
-//         for (let j = i + 1; j < result.length; j++) {
-//             if (i === j) return;
-//             if (JSON.stringify(clq.core) === JSON.stringify(result[j].core)) {
-//                 result[j].affiliates = result[j].affiliates.concat(clq.affiliates);
-//                 result.splice(i, 1);
-//                 j--;
-//             }
-//         }
-//     });
-//     // sort result (again...)
-//     result.sort((a, b) => b.affiliates.length + b.core.length - (a.affiliates.length + a.core.length));
-//     // sort cliques within the clique themselves
-//     result.forEach((clique) => {
-//         let clique_popularity: { [id: number]: number } = {};
-//         clique.core.sort((a, b) => {
-//             const popularity_a = isWellDefined(clique_popularity[a])
-//                 ? clique_popularity[a]
-//                 : _calculatePopularity(a, clique.core.concat(clique.affiliates), gameState);
-//             clique_popularity[a] = popularity_a;
-//             //
-//             const popularity_b = isWellDefined(clique_popularity[b])
-//                 ? clique_popularity[b]
-//                 : _calculatePopularity(b, clique.core.concat(clique.affiliates), gameState);
-//             clique_popularity[b] = popularity_b;
-//             return b - a;
-//         });
-//         clique_popularity = {};
-//     });
-//     return result;
-// }
-
-function _calculatePopularity(hero: number, clique: number[], gameState: GameState) {
-    let sum = 0;
-    let count = 0;
-    clique.forEach((houseguest) => {
-        if (houseguest !== hero) {
-            count++;
-            sum += getById(gameState, hero).relationships[hero];
-        }
-    });
-    return count === 0 ? 0 : sum / count;
 }
 
 function bronKerbosch(r: Set<number>, p: Set<number>, x: Set<number>, g: Graph) {
