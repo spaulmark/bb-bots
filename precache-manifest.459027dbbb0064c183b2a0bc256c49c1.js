fb3bdf85ea332d273605ac051f9ab346408bcadb
Spencer <fdoou1@gmail.com>
2022-03-27 01:55:22 -0400

    Updates

--- precache-manifest.459027dbbb0064c183b2a0bc256c49c1.js
+++ precache-manifest.459027dbbb0064c183b2a0bc256c49c1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "10c81164e40c139138fd",
+    "revision": "2b40d782543665fcca05",
     "url": "/bb-bots/static/css/main.cc3ff623.chunk.css"
   },
   {
-    "revision": "10c81164e40c139138fd",
-    "url": "/bb-bots/static/js/main.15f88a13.chunk.js"
+    "revision": "2b40d782543665fcca05",
+    "url": "/bb-bots/static/js/main.71300ff5.chunk.js"
   },
   {
     "revision": "0a78736eeef7e5f831be",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/bb-bots/static/js/2.a76014b7.chunk.js"
   },
   {
-    "revision": "42437f51bcab67c1776d11298b46d5d9",
+    "revision": "c91468821fda68532616e93acf5c6299",
     "url": "/bb-bots/index.html"
   }
 ];
